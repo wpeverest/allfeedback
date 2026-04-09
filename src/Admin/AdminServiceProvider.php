1753d2b3f2d04ba207215f33bb20088d653105ac
rg755421 <rg755421@gmail.com>
2026-04-09 13:21:38 +0545

    fix: use white fill for admin sidebar SVG icon to prevent black flash on load

--- src/Admin/AdminServiceProvider.php
+++ src/Admin/AdminServiceProvider.php
@@ -73,7 +73,7 @@ class AdminServiceProvider implements ServiceProvider {
 
 		$menu_icon = 'data:image/svg+xml;base64,' . base64_encode(
 			'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">'
-			. '<path fill="black" d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>'
+			. '<path fill="white" d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>'
 			. '</svg>'
 		);
 
